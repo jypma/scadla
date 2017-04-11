3cb237c2849b6bcd1cf1a173f82458a206f5d69e
jypma <jan@ypmania.nl>
2017-04-11 15:52:35 +0200

    change bevels to be round, intent to use rubber

--- src/test/scala/scadla/examples/SensorMold.scala
+++ src/test/scala/scadla/examples/SensorMold.scala
@@ -21,20 +21,30 @@ class SensorMold extends FunSuite {
     val mountDepth = 4.0
     val mountHole = 1.75
     
-    val tolerance = 0.4 // hole is 0.4mm bigger than bevel 
+    val tolerance = 0.8 // hole is 0.4mm bigger than bevel 
     
     val shape = Cylinder(r + wall, height + wall) -
                 Cylinder(r, height+1).moveZ(wall)
     val bottom = shape - Cube((r+wall+1)*2, (r+wall+1)*2, wall+height+2).moveX(-r-wall-1).moveZ(-1)
     
     val b = wall / 3.0
-    val bevel = Cube(b,b+1,height+wall-b).moveY(-1).moveZ(b).moveX(r+b) +
-                Cube(b,b+1,height+wall-b).moveY(-1).moveZ(b).moveX(-r-b*2) +
-                Cube(r*2+b*4,b+1,b).moveY(-1).moveZ(b).moveX(-r-b*2)
     
-    val hole =  Cube(b+tolerance,b+0.1,height+wall-b+0.1).moveY(0.1-b).moveZ(b).moveX(r+b-0.5*tolerance) +
-                Cube(b+tolerance,b+0.1,height+wall-b+0.1).moveY(0.1-b).moveZ(b).moveX(-r-b*2-0.5*tolerance) +
-                Cube(r*2+b*4+tolerance,b+0.1,b+tolerance).moveY(0.1-b).moveZ(b-0.5*tolerance).moveX(-r-b*2-0.5*tolerance)
+    val bevelR = wall * 0.5
+    val hole = (Cylinder(bevelR, height + wall + bevelR).moveX(-r - wall*0.5).moveZ(wall/2 - bevelR) -
+                Cube(40,bevelR*2,40).rotateY(45°).moveY(-bevelR).moveX(-wall-r)) +
+               (Cylinder(bevelR, height + wall + bevelR).moveX(r + wall*0.5).moveZ(wall/2 - bevelR) -
+                Cube(40,bevelR*2,40).rotateY(-135°).moveY(-bevelR).moveX(wall+r)) +
+               (Cylinder(bevelR, r*2 + wall + 2*bevelR).rotateY(90°).moveX(-r - wall/2 - bevelR).moveZ(wall/2) -
+                Cube(40,bevelR*2,40).rotateY(-45°).moveY(-bevelR).moveX(wall+r) -
+                Cube(40,bevelR*2,40).rotateY(-45°).moveY(-bevelR).moveX(-wall-r))
+    
+    //val bevel = Cube(b,b+1,height+wall-b).moveY(-1).moveZ(b).moveX(r+b) +
+    //            Cube(b,b+1,height+wall-b).moveY(-1).moveZ(b).moveX(-r-b*2) +
+    //            Cube(r*2+b*4,b+1,b).moveY(-1).moveZ(b).moveX(-r-b*2)
+    
+    //val hole =  Cube(b+tolerance,b+0.1,height+wall-b+0.1).moveY(0.1-b).moveZ(b).moveX(r+b-0.5*tolerance) +
+    //            Cube(b+tolerance,b+0.1,height+wall-b+0.1).moveY(0.1-b).moveZ(b).moveX(-r-b*2-0.5*tolerance) +
+    //            Cube(r*2+b*4+tolerance,b+0.1,b+tolerance).moveY(0.1-b).moveZ(b-0.5*tolerance).moveX(-r-b*2-0.5*tolerance)
                 
     val mount = (
       Cube(mountSize, mountDepth, mountSize) - 
@@ -50,10 +60,12 @@ class SensorMold extends FunSuite {
                  mount.moveX(r+wall+mountSize/2-1).moveZ(height*6/7) +
                  mount.moveX(-(r+wall+mountSize/2-1)).moveZ(height*6/7)
 
-    val foot = Cylinder(0.5*mountSize, mountSize+0.1).moveZ(-mountSize).moveY(-r)                 
+    val foot = Cube(mountSize, height/3.0, mountSize + 0.1).moveZ(-mountSize).moveY(-height/3.0-r+wall).moveX(-mountSize/2) +
+               Cube(height/3.0, mountSize, mountSize + 0.1).moveZ(-mountSize).moveY(-mountSize-1).moveX(r-wall) +
+               Cube(height/3.0, mountSize, mountSize + 0.1).moveZ(-mountSize).moveY(-mountSize-1).moveX(-height/3.0-r+wall)
                  
-    OpenSCAD.toSCAD(bottom + foot + mounts + bevel, "two")
-    OpenSCAD.toSTL(bottom + foot + mounts + bevel, "two.stl")
+    OpenSCAD.toSCAD(bottom + foot + mounts + hole, "two")
+    OpenSCAD.toSTL(bottom + foot + mounts + hole, "two.stl")
     OpenSCAD.toSCAD(bottom + foot + mounts - hole, "out")
     OpenSCAD.toSTL(bottom + foot + mounts - hole, "one.stl")
   }
